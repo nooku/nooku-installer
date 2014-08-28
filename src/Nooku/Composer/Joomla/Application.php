efeb421a36c44bb4b401da8ed53431f210d3d25a
Steven Rombauts <steven@timble.net>
2014-08-29 00:08:34 +0200

    Clean-up: convert tabs to spaces in docblocks

--- src/Nooku/Composer/Joomla/Application.php
+++ src/Nooku/Composer/Joomla/Application.php
@@ -2,9 +2,9 @@
 /**
  * Nooku Installer plugin - https://github.com/nooku/nooku-composer
  *
- * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		https://github.com/nooku/nooku-composer for the canonical source repository
+ * @copyright    Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license      GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link         https://github.com/nooku/nooku-composer for the canonical source repository
  */
 
 namespace Nooku\Composer\Joomla;
